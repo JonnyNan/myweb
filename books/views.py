2a8d2703b426d241c4eddbfdfd94aff36ce53c86
JonnyNan <nanzhang1211@outlook.com>
2016-09-12 23:33:29 +0800

    update views.py

--- books/views.py
+++ books/views.py
@@ -6,18 +6,19 @@ from django.shortcuts import render_to_response
 from django.shortcuts import render
 from django.shortcuts import HttpResponseRedirect
 
-
 # Create your views here.
 
-# sutdent list
 from books.models import *
+
 
 def table(request):
     lagous = Result.objects.all()
     return render_to_response('table.html', {'lagou_list': lagous})
 
+
 def search(request):
-    return render(request,'search.html')
+    return render(request, 'search.html')
+
 
 def search_lagou(request):
     username = request.POST.get('searchbox', '')
@@ -25,13 +26,6 @@ def search_lagou(request):
     response = HttpResponseRedirect('/table/')
     return response
 
-connection = pymysql.connect(host='127.0.0.1',
-                             port=3306,
-                             user='root',
-                             password='123456',
-                             db='lagou',
-                             charset='utf8',
-                             cursorclass=pymysql.cursors.DictCursor)
 
 def get_json(url, page, lang_name):
     data = {'first': 'true', 'pn': page, 'kd': lang_name}
@@ -39,20 +33,20 @@ def get_json(url, page, lang_name):
     list_con = json['content']['positionResult']['result']
     info_list = []
     for i in list_con:
-        info = []
-        info.append(i['companyShortName'])
-        info.append(i['companySize'])
-        info.append(i['positionName'])
-        info.append(i['salary'])
-        info.append(i['positionAdvantage'])
-        info.append(i['workYear'])
-        info.append(i['city'])
-        info.append(i['education'])
-        info.append(i['createTime'])
+        info = [i['companyShortName'], i['companySize'], i['positionName'], i['salary'], i['positionAdvantage'],
+                i['workYear'], i['city'], i['education'], i['createTime']]
         info_list.append(info)
     return info_list
 
+
 def main(name):
+    connection = pymysql.connect(host='127.0.0.1',
+                                 port=3306,
+                                 user='root',
+                                 password='123456',
+                                 db='lagou',
+                                 charset='utf8',
+                                 cursorclass=pymysql.cursors.DictCursor)
     lang_name = name
     page = 0
     url = 'http://www.lagou.com/jobs/positionAjax.json?needAddtionalResult=false'
@@ -68,14 +62,14 @@ def main(name):
         with connection.cursor() as cursor:
             # Create a new record
             for row in info_result:
-                sql = "INSERT INTO `books_result` (`companyShortName`, `companySize`,`positionName`,`salary`,`positionAdvantage`,`workYear`," \
+                sql = "INSERT INTO `books_result` (`companyShortName`, `companySize`,`positionName`,`salary`," \
+                      " `positionAdvantage`,`workYear`," \
                       "`city`,`education`,`createTime`) VALUES (%s, %s, %s,%s,%s, %s, %s,%s,%s)"
-                list = []
-                list.append(row)
-                createTime = list[0][8].split(" ")
-                list[0].pop()
-                list[0].append(createTime[0])
-                cursor.execute(sql, (list[0][0:9]))
+                job_list = [row]
+                create_time = job_list[0][8].split(" ")
+                job_list[0].pop()
+                job_list[0].append(create_time[0])
+                cursor.execute(sql, (job_list[0][0:9]))
 
             # connection is not autocommit by default. So you must commit to save
             # your changes.
@@ -84,7 +78,6 @@ def main(name):
     finally:
         connection.close()
 
-
     for row in info_result:
         ws1.append(row)
-    wb.save(lang_name+'.xlsx')
+    wb.save(lang_name + '.xlsx')
